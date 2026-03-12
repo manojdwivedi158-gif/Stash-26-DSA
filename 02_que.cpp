5748cdbaa6d904aa094c5ed2441b9abda566c19a
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-13 03:10:53 +0530

    basic_que

---
+++ 02_que.cpp
@@ -0,0 +1,10 @@
+//  Q2. Simple Interest Calculation 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int p=400,r=5,t=1,si;
+    si = (p*r*t)/100;
+    cout<<" simple interest:"<<si;
+
+}
---
+++ 03_que.cpp
@@ -0,0 +1,17 @@
+//   Q3. Check Whether a Number is Odd or Even 
+
+// enter your number:7
+// odd
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    cout<<"enter your number:";
+    cin>>i;
+    if(i%2==0){
+        cout<<"even";
+    }
+    else cout<<"odd";
+}
