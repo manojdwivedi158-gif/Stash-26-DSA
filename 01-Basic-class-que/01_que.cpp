a8f939ddbd0303b980a3b5dd3b8fc0022afb7720
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-13 02:55:29 +0530

    basic_que

---
+++ 01-Basic-class-que/01_que.cpp
@@ -0,0 +1,16 @@
+// Question: a Write a program to dd two numbers:
+
+// enter your number:4
+// enter your second number:5
+// The sum of 4 and 5 is : 9
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    cout<<"enter your number:";
+    cin>>i;
+    cout<<"enter your second number:";
+    cin>>j;
+    cout<<"The sum of "<<i<<" "<<"and "<<j<<" is : "<<i+j;
+}
