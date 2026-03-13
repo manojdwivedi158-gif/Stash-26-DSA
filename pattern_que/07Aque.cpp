4d55b482a7e485293f61afb3e5add74df220a094
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-14 05:26:29 +0530

    pattern_que

---
+++ pattern_que/07Aque.cpp
@@ -0,0 +1,29 @@
+
+
+//  enter your number:5
+//      **
+//     ****
+//    ******
+//   ********
+//  **********
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i*2;j++){
+            cout<<"*";
+        }
+         cout<<endl;
+    }
+    
+}
