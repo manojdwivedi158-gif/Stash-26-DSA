bab37d86b804d8a040b1c08eb5b0887790337781
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-14 01:00:37 +0530

    pattern_que

---
+++ pattern_que/01_que.cpp
@@ -0,0 +1,17 @@
+//
+//  for n=5
+//  * * * * * 
+//
+  
+
+  #include<iostream>
+  using namespace std;
+  int main(){
+    int n;
+    cout<<"enter tour number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"* ";
+    }
+    
+    #include<iostream>
